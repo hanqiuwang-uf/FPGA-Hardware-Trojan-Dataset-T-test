e614f19b3587cae6cbad34878942256f64684cb3
ACce1er4t0r <aslin@Aslinde-MacBook-Pro.local>
2021-11-05 05:19:05 -0400

    Fixing errors

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import scipy.stats
 import matplotlib.pyplot as plt
-import matplotlib
 import numpy as np
 import h5py
 
@@ -12,19 +11,10 @@ def Ttest250(inputfile, outputfile="Ttest250.trcs"):
     The original version of the t-test, which calculates
     the maximum value of the 10*10 sets of 250 data
     '''
-    # Test to see if it is welch's t test
-    # a = [17.2, 20.9, 22.6, 18.1, 21.7, 21.4, 23.5, 24.2, 14.7, 21.8]
-    # b = [21.5, 22.8, 21.0, 23.0, 21.6, 23.6, 22.5, 20.7, 23.4, 21.8, 20.7, 21.7, 21.5, 22.5, 23.6, 21.5, 22.5, 23.5, 21.5, 21.8]
-    # c = scipy.stats.ttest_ind(a, b, equal_var=False)
-
-    # read file
     hf = h5py.File(inputfile, 'r')
-    # index
     xlab = list(hf.keys())
-    # Since the subscripts under all secondary arrays are the same, just use the first one
     ylab = list(hf[xlab[0]].keys())
 
-    k = 0
     l = []
     m = []
     f = [[] for x in range(10)]
@@ -102,91 +92,101 @@ def Ttest249(inputfile, outputfile="Ttest249.trcs"):
 def ED(inputfile):
     '''
     :param inputfile: Input dataset filename
-    :return: dist_list_list: A 10*10*250 list
     Calculate the Euclidean distance between random and
     semifixed if there is only one file input, and
-    finally return a data of 10*10*250
+    finally plot the data
     '''
     hf = h5py.File(inputfile, 'r')
     xlab = list(hf.keys())
     ylab = list(hf[xlab[0]].keys())
-    # TVLA = list(hf[xlab[0]][ylab[0]].keys())[0]
-    # lab = list(hf[xlab[0]][ylab[0]][TVLA].keys())[1]
-    x = len(hf[xlab[0]][ylab[0]]['TVLA']['random'])     # 250
-    y = len(hf[xlab[0]][ylab[0]]['TVLA']['random'][0])  # 1000
-    # print()
-    dist = []
-    dist_list = []
-    dist_list_list = []
+
+    r, s = [], []
+    r_list, s_list = [], []
+    r_list_list, s_list_list = [], []
     for i in xlab:
         for j in ylab:
-            random = np.array(hf[i][j]['TVLA']['random'][:])
-            semifixed = np.array(hf[i][j]['TVLA']['semifixed'][:])
+            random = np.array(hf[i][j]['TVLA']['random'][:-1])
+            semifixed = np.array(hf[i][j]['TVLA']['semifixed'][:-1])
+
+            tmp_r = [sum(x) / len(random[0]) for x in zip(*random)]
+            tmp_s = [sum(x) / len(semifixed[0]) for x in zip(*semifixed)]
+
             for k in range(len(random)):
-                dist.append(np.linalg.norm(random[k]-semifixed[k]))
-            dist_list.append(dist)
-            dist = []
-        dist_list_list.append(dist_list)
-        dist_list = []
-    # Final dist_list_list is a 10*10*250 array
-    return dist_list_list
+                r.append(np.linalg.norm(random[k] - tmp_r))
+                s.append(np.linalg.norm(semifixed[k] - tmp_s))
+            r_list.append(r)
+            s_list.append(s)
+            r, s = [], []
+        r_list_list.append(r_list)
+        s_list_list.append(s_list)
+        r_list, s_list = [], []
+    plt.figure(figsize=(40,40))
+    kwargs = dict(histtype='stepfilled', alpha=0.3, density=True, bins=40)
+    for i in range(len(r_list_list)):
+        for j in range(len(r_list_list[0])):
+            plt.subplot(10, 10, i*10+j+1)
+            plt.hist(r_list_list[i][j], **kwargs)
+            plt.hist(s_list_list[i][j], **kwargs)
+    plt.show()
 
 
-def ED2(inputfile1, inputfile2):
+
+def TtestMultiFile(inputfile1, inputfile2, flag='R_R'):
     '''
-    :param inputfile1: First dataset filename
-    :param inputfile2: Second dataset filename
-    :return random_list_list: A 10*10*250 data
-    :return semifixed_list_list: A 10*10*250 data
-    If there are two files input, calculate the Euclidean distance
-    between random and semifixed of different files, and
-    finally return two 10*10*250 data
+    :param inputfile1:
+    :param inputfile2:
+    :param flag: 'R_R' means t-test between different datasets random data with random data
+                 'R_S' means t-test between different datasets of random data and semifixed data
     '''
-    # First dataset
     hf1 = h5py.File(inputfile1, 'r')
     xlab1 = list(hf1.keys())
     ylab1 = list(hf1[xlab1[0]].keys())
-    # TVLA = list(hf1[xlab1[0]][ylab1[0]].keys())[0]
-    # lab = list(hf1[xlab1[0]][ylab1[0]][TVLA].keys())[1]
-    x1 = len(hf1[xlab1[0]][ylab1[0]]['TVLA']['random'])     # 250
-    y1 = len(hf1[xlab1[0]][ylab1[0]]['TVLA']['random'][0])  # 1000 or 2000
 
-    # Seconf dataset
     hf2 = h5py.File(inputfile2, 'r')
     xlab2 = list(hf2.keys())
     ylab2 = list(hf2[xlab2[0]].keys())
-    # TVLA = list(hf2[xlab2[0]][ylab2[0]].keys())[0]
-    # lab = list(hf2[xlab2[0]][ylab2[0]][TVLA].keys())[1]
-    x2 = len(hf2[xlab2[0]][ylab2[0]]['TVLA']['random'])     # 250
-    y2 = len(hf2[xlab2[0]][ylab2[0]]['TVLA']['random'][0])  # 1000 or 2000
 
-    random = []
-    random_list = []
-    random_list_list = []
-    semifixed = []
-    semifixed_list = []
-    semifixed_list_list = []
-
+    l1, l2 = [], []
+    m1, m2 = [], []
+    f1, f2 = [[] for x in range(10)], [[] for x in range(10)]
     for i in range(len(xlab1)):
         for j in range(len(ylab1)):
-            random1 = np.array(hf1[xlab1[i]][ylab1[j]]['TVLA']['random'][:])
-            random2 = np.array(hf2[xlab2[i]][ylab2[j]]['TVLA']['random'][:])
-            semifixed1 = np.array(hf1[xlab1[i]][ylab1[j]]['TVLA']['semifixed'][:])
-            semifixed2 = np.array(hf2[xlab2[i]][ylab2[j]]['TVLA']['semifixed'][:])
+            random1 = np.array(hf1[xlab1[i]][ylab1[j]]['TVLA']['random'][:-1])
+            random2 = np.array(hf2[xlab2[i]][ylab2[j]]['TVLA']['random'][:-1])
+            semifixed1 = np.array(hf1[xlab1[i]][ylab1[j]]['TVLA']['semifixed'][:-1])
+            semifixed2 = np.array(hf2[xlab2[i]][ylab2[j]]['TVLA']['semifixed'][:-1])
             for k in range(len(random1)):
-                random.append(np.linalg.norm(random1[k]-random2[k]))
-                semifixed.append(np.linalg.norm(semifixed1[k]-semifixed2[k]))
-            random_list.append(random)
-            semifixed_list.append(semifixed)
-            random = []
-            semifixed = []
-        random_list_list.append(random_list)
-        semifixed_list_list.append(semifixed_list)
-        random_list = []
-        semifixed_list = []
-    return random_list_list, semifixed_list_list
+                if flag == 'R_R':
+                    l1.append(scipy.stats.ttest_ind(random1[k], random2[k], equal_var=False)[0])
+                    l2.append(scipy.stats.ttest_ind(semifixed1[k], semifixed2[k], equal_var=False)[0])
+                elif flag == 'R_S':
+                    l1.append(scipy.stats.ttest_ind(random1[k], semifixed2[k], equal_var=False)[0])
+                    l2.append(scipy.stats.ttest_ind(semifixed1[k], random2[k], equal_var=False)[0])
+            m1.append(max(l1))
+            m2.append(max(l2))
+            l1 = []
+            l2 = []
+
+    for i in range(10):
+        f1[i] = m1[10 * i:10 * i + 9]
+        f2[i] = m2[10 * i:10 * i + 9]
+
+    n1, n2 = np.array(f1), np.array(f2)
+    print(n1, n2)
+    plt.subplot(121)
+    plt.imshow(n1, cmap="YlGnBu", interpolation='nearest')
+    plt.colorbar()
+    plt.subplot(122)
+    plt.imshow(n2, cmap="YlGnBu", interpolation='nearest')
+    plt.colorbar()
+    plt.show()
 
 
 if __name__ == '__main__':
-    # dist = ED(inputfile='SakuraG-A-LUT-10GSs-Trojan_active-new-11-10x10-250.trcs')
-    random, semifixed = ED2(inputfile1='SakuraG-A-LUT-10GSs-Trojan_active-new-11-10x10-250.trcs', inputfile2='SakuraG-A-LUT-10GSs-Trojan_active-new-11-10x10-250.trcs')
+    print("test")
+    # Ttest250('xxx.trcs', 'Ttest250.trcs')
+    # Ttest249('xxx.trcs', 'Ttest249.trcs')
+    # ED(inputfile='xxx.trcs')
+    # TtestMultiFile(inputfile1='xxx.trcs',
+    #                inputfile2='xxx.trcs',
+    #                flag='R_S')
